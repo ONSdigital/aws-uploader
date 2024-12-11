465cfa9a4f810675f06b6ea5b74661a04a15dfe6
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2024-12-11 15:07:27 +0000

    changes to origin access control

--- cloudfront.tf
+++ cloudfront.tf
@@ -24,6 +24,14 @@ resource "aws_cloudfront_origin_access_identity" "oai" {
   comment = "OAI for website"
 }
 
+resource "aws_cloudfront_origin_access_control" "cloudfront" {
+  name                              = "cloudfront"
+  description                       = "CloudFront Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   #checkov:skip=CKV_AWS_86:testing cloudfront, fix to be implemented
@@ -35,7 +43,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   #checkov:skip=CKV2_AWS_47:testing cloudfront, fix to be implemented
   origin {
     domain_name              = data.aws_s3_bucket.upload_bucket.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_identity.oai.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront.id
     origin_id                = "S3Origin"
   }
 
