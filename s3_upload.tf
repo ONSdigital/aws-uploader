30c3219c08d1903f229ceea68a51d6d03630ba37
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2024-12-13 17:04:26 +0000

    added terraform for rest api gateway

--- s3_upload.tf
+++ s3_upload.tf
@@ -1,7 +1,7 @@
 module "ons_upload_ingest_bucket" {
   #checkov:skip=CKV_TF_1:using versioning instead of git commit hashes
   source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=v6.1.0"
-  bucket_name = var.upload_host_bucket_name
+  bucket_name = var.upload_ingest_bucket_name
   versioning  = true
   tiering     = false
   logging     = false
