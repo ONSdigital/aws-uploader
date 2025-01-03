1026db2526ab33973c8b0c5779b886305e3ae501
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-03 16:12:46 +0000

    checkov fixes

--- api_gateway.tf
+++ api_gateway.tf
@@ -8,6 +8,10 @@ resource "aws_apigatewayv2_stage" "api" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
+  access_log_settings {
+   destination_arn = "${aws_cloudwatch_log_group.example.arn}"
+   format          = "..."
+  }
 }
 
 # Create GET route
