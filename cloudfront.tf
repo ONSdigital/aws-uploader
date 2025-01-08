636f6f63bad692d2d2e1d061957ccd8f06c2c271
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-08 13:51:18 +0000

    added certificate for dns

--- cloudfront.tf
+++ cloudfront.tf
@@ -55,8 +55,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   default_root_object = "index.html"
   logging_config { #CKV_AWS_86
-    bucket = "${module.cloudfront_logging_bucket.bucket_id}.s3.amazonaws.com"
+    bucket = aws_s3_bucket.cloudfront_logging_bucket.bucket_domain_name
     prefix = "logging"
+    
 
   }
 
