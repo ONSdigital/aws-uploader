87eb9237bd64af1b9d39b61ff887e60ff0dcc966
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-08 13:12:06 +0000

    added route 53

--- route_53.tf
+++ route_53.tf
@@ -1,11 +1,17 @@
-resource "aws_route53_zone" "primary" {
-  name = "uploader.com"
+
+data "aws_route53_zone" "domain" {
+  name = var.domain_name
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.uploader.com"
+resource "aws_route53_record" "uploader" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "upload.${var.domain_name}"
   type    = "A"
   ttl     = 300
-  records = [aws_eip.lb.public_ip]
+  
+  alias {
+    name = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
