2f143725f214bea41aac50eeafc6b2b334277c73
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-09 14:57:30 +0000

    changes to the bucket policy

--- s3_host.tf
+++ s3_host.tf
@@ -64,7 +64,7 @@ resource "aws_s3_bucket_website_configuration" "ons_upload_configuration" {
   bucket = module.ons_upload_bucket.bucket_id
 
   index_document {
-    suffix = "council-tax/index.html"
+    suffix = "${aws_s3_object.council_tax_folder.key}index.html"
   }
 }
 
