a20787ecc1464ef7f44249e82a73b80c3dd7271c
Nik Webb <nik.webb@ons.gov.uk>
2025-01-09 09:03:43 +0000

    wrong bucket

--- s3_host.tf
+++ s3_host.tf
@@ -2,14 +2,58 @@
 #tfsec:ignore:aws-s3-enable-versioning
 module "ons_upload_bucket" {
   #checkov:skip=CKV_TF_1:using versioning instead of git commit hashes
-  source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=v6.1.0"
+  source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=v6.0.0"
   bucket_name = var.upload_host_bucket_name
   versioning  = true
   tiering     = false
   logging     = false
 
-  attach_secure_transport_policy = true
+  attach_secure_transport_policy = false
 
+}
+
+data "aws_iam_policy_document" "uploader_bucket" {
+  statement {
+    effect = "Allow"
+
+    actions   = ["s3:GetObject"]
+    resources = ["${module.ons_upload_bucket.bucket_arn}/*"]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "cloudfront.amazonaws.com"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"]
+    }
+  }
+  statement {
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [module.ons_upload_bucket.bucket_arn,
+      "${module.ons_upload_bucket.bucket_arn}/*"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "uploader_bucket" {
+  bucket = module.ons_upload_bucket.bucket_id
+  policy = data.aws_iam_policy_document.uploader_bucket.json
 }
 
 data "aws_s3_bucket" "upload_bucket" {
--- s3_upload.tf
+++ s3_upload.tf
@@ -6,48 +6,6 @@ module "ons_upload_ingest_bucket" {
   tiering     = false
   logging     = false
 
-  attach_secure_transport_policy = false
-}
-data "aws_iam_policy_document" "uploader_bucket" {
-  statement {
-    effect = "Allow"
-
-    actions   = ["s3:GetObject"]
-    resources = ["${module.ons_upload_bucket.bucket_arn}/*"]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "cloudfront.amazonaws.com"
-      ]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = ["arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"]
-    }
-  }
-  statement {
-    effect  = "Deny"
-    actions = ["s3:*"]
-    resources = [module.ons_upload_bucket.bucket_arn,
-      "${module.ons_upload_bucket.bucket_arn}/*"
-    ]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["false"]
-    }
-  }
+  attach_secure_transport_policy = true
 }
 
-
-resource "aws_s3_bucket_policy" "uploader_bucket" {
-  bucket = module.ons_upload_bucket.bucket_id
-  policy = data.aws_iam_policy_document.uploader_bucket.json
-}
