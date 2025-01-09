71066537ac4bcebbd6d0886f3db9f2751e7d453c
Nik Webb <nik.webb@ons.gov.uk>
2025-01-09 08:46:26 +0000

    downgrading module as bug

--- s3_upload.tf
+++ s3_upload.tf
@@ -1,13 +1,12 @@
 module "ons_upload_ingest_bucket" {
   #checkov:skip=CKV_TF_1:using versioning instead of git commit hashes
-  source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=v6.1.0"
+  source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=v6.0.0"
   bucket_name = var.upload_ingest_bucket_name
   versioning  = true
   tiering     = false
   logging     = false
 
   attach_secure_transport_policy = false
-
 }
 data "aws_iam_policy_document" "uploader_bucket" {
   statement {
