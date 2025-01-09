4ec34118028b549b1f6f6293721343ae4eb6eca8
Nik Webb <nik.webb@ons.gov.uk>
2025-01-09 08:21:03 +0000

    updating bucket policy

--- s3_upload.tf
+++ s3_upload.tf
@@ -6,6 +6,74 @@ module "ons_upload_ingest_bucket" {
   tiering     = false
   logging     = false
 
-  attach_secure_transport_policy = true
+  attach_secure_transport_policy = false
 
+}
+
+resource "aws_s3_bucket_policy" "ons_upload_policy" {
+  bucket = module.ons_upload_bucket.bucket_id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontServicePrincipalReadOnly",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::aws-uploader-ost-dev/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : "arn:aws:cloudfront::055232432732:distribution/E28DWVPYYHYB4P"
+          }
+        }
+      }
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "uploader_bucket" {
+  statement {
+    effect = "Allow"
+
+    actions   = ["s3:GetObject"]
+    resources = ["${module.ons_upload_bucket.bucket_arn}/*"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "cloudfront.amazonaws.com"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.ons_uploader.id}"]
+    }
+  }
+  statement {
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [module.ons_upload_bucket.bucket_arn,
+      "${module.ons_upload_bucket.bucket_arn}/*"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "uploader_bucket" {
+  bucket = module.ons_upload_bucket.bucket_arn
+  policy = data.aws_iam_policy_document.uploader_bucket.json
 }
