f112fbdbd651b7c8d3ff88495b3be698e819a3c4
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-10 11:54:32 +0000

    added council html pages

--- s3_host.tf
+++ s3_host.tf
@@ -113,3 +113,19 @@ resource "aws_s3_object" "file_names_dont_match_page" {
   source_hash = filemd5("${path.module}/scripts/file_names_dont_match_error.html")
   content_type = "text/html"
 }
+
+resource "aws_s3_object" "012345678-council" {
+  bucket      = module.ons_upload_bucket.bucket_id
+  key         = "council-tax/012345678-council.html"
+  source      = "${path.module}/scripts/012345678-council.html"
+  source_hash = filemd5("${path.module}/scripts/012345678-council.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "012345678-council2" {
+  bucket      = module.ons_upload_bucket.bucket_id
+  key         = "council-tax/012345678-council2.html"
+  source      = "${path.module}/scripts/012345678-council2.html"
+  source_hash = filemd5("${path.module}/scripts/012345678-council2.html")
+  content_type = "text/html"
+}
