246bc12b8c87ee1f75dedbf98a5b6e6c9a75973b
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-14 11:21:34 +0000

    added cors policies

--- api_gateway.tf
+++ api_gateway.tf
@@ -5,7 +5,7 @@ resource "aws_apigatewayv2_api" "api" {
     allow_origins = [local.website_address]
     allow_methods = ["GET", "OPTIONS"]
     allow_headers = ["Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token"]
-    max_age = 300
+    max_age       = 300
   }
 }
 
@@ -33,6 +33,7 @@ resource "aws_apigatewayv2_route" "options" {
   api_id             = aws_apigatewayv2_api.api.id
   route_key          = "OPTIONS /pre-signed-url"
   target             = "integrations/${aws_apigatewayv2_integration.api.id}"
+  authorization_type = "AWS_IAM"
 }
 
 resource "aws_apigatewayv2_integration" "api" {
