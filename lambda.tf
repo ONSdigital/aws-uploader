3be52f91f6bf33660a023ca10a9a112a0e9b2f6c
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-15 15:17:32 +0000

    changed lambda handler

--- lambda.tf
+++ lambda.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 data "archive_file" "PreSignedURL" {
   type        = "zip"
-  source_file = "${path.module}/src/PreSignedURL.js"
+  source_file = "${path.module}/src/PreSignedURL.mjs"
   output_path = "${path.module}/PreSignedURL.zip"
 }
 
