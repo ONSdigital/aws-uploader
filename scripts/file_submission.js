636a546bf32e5a71ee2712800772160288ae53fe
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-16 15:47:11 +0000

    added env variables

--- scripts/file_submission.js
+++ scripts/file_submission.js
@@ -1,5 +1,5 @@
 
-const url = process.env.upload_url; //API Gateway URL. Once API GAteway is called, the lambda is triggered which 
+const url = env.upload_url; //API Gateway URL. Once API GAteway is called, the lambda is triggered which 
 //carried out file validation and returns pre-signed URLs if files pass checks
 
 const options = {
