f7469f6d5f82ae013cbf8b827e4f15a139645f6b
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-16 16:07:10 +0000

    added env variables

--- scripts/file_submission.js
+++ scripts/file_submission.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 
-const url = env.upload_url; //API Gateway URL. Once API GAteway is called, the lambda is triggered which 
+const url = process.env.upload_url; //API Gateway URL. Once API GAteway is called, the lambda is triggered which 
 //carried out file validation and returns pre-signed URLs if files pass checks
 
 const options = {
