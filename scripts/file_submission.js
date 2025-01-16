2002ee0e8a6533f29ad5602d49525dc5792a4f31
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-16 15:38:32 +0000

    added env variables

--- scripts/file_submission.js
+++ scripts/file_submission.js
@@ -1,5 +1,5 @@
 
-const url = 'https://bcvwyrcdub.execute-api.eu-west-2.amazonaws.com/pre-signed-url'; //API Gateway URL. Once API GAteway is called, the lambda is triggered which 
+const url = process.env.upload_url; //API Gateway URL. Once API GAteway is called, the lambda is triggered which 
 //carried out file validation and returns pre-signed URLs if files pass checks
 
 const options = {
