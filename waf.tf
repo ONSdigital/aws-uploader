f2b6433af7f5400a147e47e7fe3940955e68ce58
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-21 10:37:23 +0000

    waf config changes

--- waf.tf
+++ waf.tf
@@ -11,13 +11,13 @@ resource "aws_wafv2_web_acl" "waf_cloudfront" {
     allow {}
   }
 
-  association_config {
-    request_body {
-      api_gateway {
-        default_size_inspection_limit = "KB_16"
-      }
-    }
-  }
+  # association_config {
+  #   request_body {
+  #     api_gateway {
+  #       default_size_inspection_limit = "KB_16"
+  #     }
+  #   }
+  # }
 
   rule {
     name     = "AWS-AWSManagedRulesAmazonIpReputationList"
