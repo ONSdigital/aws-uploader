d6f7ab9c0671b712fd13c649b9d61bda20bd9c67
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-23 11:21:27 +0000

    lambda changes

--- s3_host.tf
+++ s3_host.tf
@@ -147,10 +147,3 @@ resource "aws_s3_object" "result_message" {
   content_type = "text"
 }
 
-resource "aws_s3_object" "logger" {
-  bucket       = module.ons_upload_bucket.bucket_id
-  key          = "council-tax/logger.js"
-  source       = "${path.module}/src/utilities/logger.js"
-  source_hash  = filemd5("${path.module}/src/utilities/logger.js")
-  content_type = "text"
-}
