89076b489ef152cd81ef468f9f208681ac4b06fc
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-01-29 14:50:47 +0000

    made input fields id's unique and added CSP Header

--- cloudfront.tf
+++ cloudfront.tf
@@ -43,7 +43,7 @@ resource "aws_cloudfront_distribution" "uploader" {
     cached_methods             = ["GET", "HEAD"]
     target_origin_id           = "S3Origin"
     origin_request_policy_id   = "acba4595-bd28-49b8-b9fe-13317c0390fa" # Managed-CORS-CustomOrigin policy ID
-    response_headers_policy_id = "60669652-455b-4ae9-85a4-c4c02393f86c"
+    response_headers_policy_id = "eaab4381-ed33-4a86-88ca-d9558dc6cd63"
     cache_policy_id            = "658327ea-f89d-4fab-a63d-7e88639e58f6"
 
     # forwarded_values {
