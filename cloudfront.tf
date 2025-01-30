fc1693db4f2875ba4583551b81b6d00f92f042ae
Nik Webb <nik.webb@ons.gov.uk>
2025-01-30 09:02:51 +0000

    allow origins

--- cloudfront.tf
+++ cloudfront.tf
@@ -95,7 +95,7 @@ resource "aws_cloudfront_response_headers_policy" "custom_security_headers" {
     }
     
     access_control_allow_origins {
-      items = ["https://${local.website_address}", local.formatted_api_url]
+      items = ["*"]
     }
     
     origin_override = true
