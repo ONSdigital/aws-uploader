890d3386321188635615b26d0ff604bf7794fbbe
Nik Webb <nik.webb@ons.gov.uk>
2025-02-03 12:22:37 +0000

    testing module

--- s3_host.tf
+++ s3_host.tf
@@ -2,8 +2,8 @@
 #tfsec:ignore:aws-s3-enable-versioning
 module "ons_upload_bucket" {
   #checkov:skip=CKV_TF_1:using versioning instead of git commit hashes
-
-  source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=v7.3.0"
+  #checkov:skip=CKV_TF_2:logging not needed
+  source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=secure-policy"
   bucket_name = var.upload_host_bucket_name
   versioning  = true
   tiering     = false
--- s3_upload.tf
+++ s3_upload.tf
@@ -1,6 +1,7 @@
 module "ons_upload_ingest_bucket" {
   #checkov:skip=CKV_TF_1:using versioning instead of git commit hashes
-  source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=v7.3.0"
+  #checkov:skip=CKV_TF_2:logging not needed
+  source      = "git::https://github.com/ONSdigital/aws-s3-bucket.git?ref=secure-policy"
   bucket_name = var.upload_ingest_bucket_name
   versioning  = false
   tiering     = false
