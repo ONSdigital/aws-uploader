0191e9bb1f3ac4f658b7603d06bbb022cd24cec3
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-02-25 14:24:53 +0000

    updating terraform plan/apply target repo

--- s3_host.tf
+++ s3_host.tf
@@ -65,12 +65,6 @@ resource "aws_s3_bucket_website_configuration" "ons_upload_configuration" {
   }
 }
 
-# resource "aws_s3_object" "council_tax_folder" {
-#   bucket  = module.ons_upload_bucket.bucket_id
-#   key     = "council-tax/"
-#   content = ""
-# }
-
 resource "aws_s3_object" "home_page" {
   bucket       = module.ons_upload_bucket.bucket_id
   key          = "index.html"
