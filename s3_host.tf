99baea040800218f65d03f292d3518649d90055a
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-02-28 11:31:50 +0000

    testing tf templating

--- s3_host.tf
+++ s3_host.tf
@@ -116,9 +116,9 @@ resource "aws_s3_object" "file_names_dont_match_page" {
 resource "aws_s3_object" "_012345678-council" {
   bucket = module.ons_upload_bucket.bucket_id
   key    = "council-tax/E12345678-council.html"
-  source = data.template_file.templating.html.rendered
+  source = "${path.module}/scripts/E12345678-council-rendered.html"
   # source       = "${path.module}/scripts/E12345678-council.html"
-  source_hash  = filemd5(data.template_file.templating.html.rendered)
+  source_hash = filemd5("${path.module}/scripts/E12345678-council-rendered.html")
   # source_hash  = filemd5("${path.module}/scripts/E12345678-council.html")
   content_type = "text/html"
 }
--- template.html.tf
+++ template.html.tf
@@ -1,10 +1,19 @@
-data "template_file" "templating_html" {
-    template = file ("${path.module}/scripts/E12345678-council.html")
-    vars = {
-        page_title = "Council Taxes"
-    }
-}
-output "rendered_html" {
-    value = data.template_file.templating.html.rendered
-    description = "this is the rendered html"
+# data "template_file" "templating_html" {
+#     template = file ("${path.module}/scripts/E12345678-council.html")
+#     vars = {
+#         page_title = "Council Taxes"
+#     }
+# }
+# output "rendered_html" {
+#     value = templatefile("${path.module}/scripts/E12345678-council.html", {
+#         page_title = "Council Taxes"
+#     })
+#     description = "this is the rendered html"
+# }
+
+resource "local_file" "rendered_html" {
+  content = templatefile("${path.module}/scripts/E12345678-council.html", {
+    page_title = "Council Taxes"
+  })
+  filename = "${path.module}/scripts/E12345678-council-rendered.html"
 }
