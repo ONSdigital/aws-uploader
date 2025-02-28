4f2d421d1c6a525d01e4510443a3d522a5280c39
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-02-28 12:08:52 +0000

    testing tf templating

--- s3_host.tf
+++ s3_host.tf
@@ -115,7 +115,7 @@ resource "aws_s3_object" "file_names_dont_match_page" {
 
 resource "aws_s3_object" "_012345678-council" {
   bucket = module.ons_upload_bucket.bucket_id
-  key    = "council-tax/E12345678-council-rendered.html"
+  key    = "council-tax/E12345678-council.html"
   source = "${path.module}/scripts/E12345678-council-rendered.html"
   # source       = "${path.module}/scripts/E12345678-council.html"
   source_hash = filemd5("${path.module}/scripts/E12345678-council.html")
