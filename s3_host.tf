fc3ffe7519134aa50b4b960eb60828a8cb130613
Nik Webb <nik.webb@ons.gov.uk>
2025-03-05 10:43:22 +0000

    removing unused files

--- s3_host.tf
+++ s3_host.tf
@@ -100,30 +100,6 @@ resource "aws_s3_object" "success_page" {
   content_type = "text/html"
 }
 
-resource "aws_s3_object" "error_page" {
-  bucket       = module.ons_upload_bucket.bucket_id
-  key          = "council-tax/error.html"
-  source       = "${path.module}/scripts/error.html"
-  source_hash  = filemd5("${path.module}/scripts/error.html")
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "not_csv_page" {
-  bucket       = module.ons_upload_bucket.bucket_id
-  key          = "council-tax/not_CSV_error.html"
-  source       = "${path.module}/scripts/not_CSV_error.html"
-  source_hash  = filemd5("${path.module}/scripts/not_CSV_error.html")
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "file_names_dont_match_page" {
-  bucket       = module.ons_upload_bucket.bucket_id
-  key          = "council-tax/file_names_dont_match_error.html"
-  source       = "${path.module}/scripts/file_names_dont_match_error.html"
-  source_hash  = filemd5("${path.module}/scripts/file_names_dont_match_error.html")
-  content_type = "text/html"
-}
-
 resource "aws_s3_object" "_012345678-council" {
   bucket       = module.ons_upload_bucket.bucket_id
   key          = "council-tax/E12345678-council.html"
@@ -137,22 +113,6 @@ resource "aws_s3_object" "newark-sherwood" {
   key          = "council-tax/E07000175-Newark&Sherwood.html"
   source_hash  = md5(local.E07000175-council-rendered-html)
   content      = local.E07000175-council-rendered-html
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "LAD_doesnt_match" {
-  bucket       = module.ons_upload_bucket.bucket_id
-  key          = "council-tax/LAD_doesnt_match.html"
-  source       = "${path.module}/scripts/LAD_doesnt_match.html"
-  source_hash  = filemd5("${path.module}/scripts/LAD_doesnt_match.html")
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "empty_file" {
-  bucket       = module.ons_upload_bucket.bucket_id
-  key          = "council-tax/empty_file_error.html"
-  source       = "${path.module}/scripts/empty_file_error.html"
-  source_hash  = filemd5("${path.module}/scripts/empty_file_error.html")
   content_type = "text/html"
 }
 
