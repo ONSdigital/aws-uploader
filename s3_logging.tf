eb713a46910f628e787823207991e7d31c936dcf
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-03-20 16:55:59 +0000

    adding athen db table

--- s3_logging.tf
+++ s3_logging.tf
@@ -82,3 +82,49 @@ resource "aws_s3_bucket_acl" "cloudfront" {
     encryption_option = "SSE_S3"
   }
  }
+
+ resource "aws_athena_named_query" "create_athena_s3_table" {
+  database  = aws_athena_database.athena-s3.name
+  name      = "create_athena_s3_logs_table"
+  query     = <<EOF
+CREATE EXTERNAL TABLE IF NOT EXISTS cloudfront_standard_logs (
+  `date` DATE,
+  time STRING,
+  x_edge_location STRING,
+  sc_bytes BIGINT,
+  c_ip STRING,
+  cs_method STRING,
+  cs_host STRING,
+  cs_uri_stem STRING,
+  sc_status INT,
+  cs_referrer STRING,
+  cs_user_agent STRING,
+  cs_uri_query STRING,
+  cs_cookie STRING,
+  x_edge_result_type STRING,
+  x_edge_request_id STRING,
+  x_host_header STRING,
+  cs_protocol STRING,
+  cs_bytes BIGINT,
+  time_taken FLOAT,
+  x_forwarded_for STRING,
+  ssl_protocol STRING,
+  ssl_cipher STRING,
+  x_edge_response_result_type STRING,
+  cs_protocol_version STRING,
+  fle_status STRING,
+  fle_encrypted_fields INT,
+  c_port INT,
+  time_to_first_byte FLOAT,
+  x_edge_detailed_result_type STRING,
+  sc_content_type STRING,
+  sc_content_len BIGINT,
+  sc_range_start BIGINT,
+  sc_range_end BIGINT
+)
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY '\t'
+LOCATION 's3://cloudfront-logging-ost-dev/'
+TBLPROPERTIES ( 'skip.header.line.count'='2' )
+EOF
+ }
