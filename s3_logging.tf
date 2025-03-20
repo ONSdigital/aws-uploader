343d4c73470666f076cf5f48f89a700e16eaf45f
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-03-20 11:34:32 +0000

    adding athena db code

--- s3_logging.tf
+++ s3_logging.tf
@@ -74,8 +74,8 @@ resource "aws_s3_bucket_acl" "cloudfront" {
 
 }
  
-#  resource "aws_athena_database" "access_logs" {
+ resource "aws_athena_database" "access_logs" {
   
-#   name = "s3_access_logs"
-#   bucket = aws_s3_bucket.cloudfront_logging_bucket.id
-#  }
+  name = "s3_access_logs"
+  bucket = aws_s3_bucket.cloudfront_logging_bucket.id
+ }
