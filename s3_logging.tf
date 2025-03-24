cd1e77902f08b0c9e5920fc3f551c4b337d27729
AndreiTurcila <andrei.turcila@ext.ons.gov.uk>
2025-03-24 13:23:16 +0000

    ran terraform fmt

--- s3_logging.tf
+++ s3_logging.tf
@@ -73,32 +73,32 @@ resource "aws_s3_bucket_acl" "cloudfront" {
   depends_on = [aws_s3_bucket_ownership_controls.cloudfront_logging_bucket]
 
 }
- 
- resource "aws_athena_database" "access_logs" {
-  name = "s3_access_logs"
+
+resource "aws_athena_database" "access_logs" {
+  name   = "s3_access_logs"
   bucket = aws_s3_bucket.cloudfront_logging_bucket.id
 
   encryption_configuration {
     encryption_option = "SSE_S3"
   }
- }
+}
 
 resource "aws_athena_workgroup" "access_logs" {
-#checkov:skip=CKV_AWS_82
-#checkov:skip=CKV_AWS_159
+  #checkov:skip=CKV_AWS_82
+  #checkov:skip=CKV_AWS_159
   name = "athena-s3"
 
   configuration {
     enforce_workgroup_configuration    = false
     publish_cloudwatch_metrics_enabled = true
-  
-  result_configuration {
-    output_location = "s3://${aws_s3_bucket.cloudfront_logging_bucket.bucket}/query_results/"
+
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.cloudfront_logging_bucket.bucket}/query_results/"
     }
   }
 }
 
- resource "aws_athena_named_query" "create_athena_s3_table" {
+resource "aws_athena_named_query" "create_athena_s3_table" {
   database  = aws_athena_database.access_logs.name
   name      = "create_athena_s3_logs_table"
   query     = <<EOF
@@ -143,4 +143,4 @@ LOCATION 's3://cloudfront-logging-ost-dev/'
 TBLPROPERTIES ( 'skip.header.line.count'='2' )
 EOF
   workgroup = aws_athena_workgroup.access_logs.name
- }
+}
