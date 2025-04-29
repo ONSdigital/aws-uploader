f858d441c7924463fa7fa435551cad94bddab712
Andrei Turcila <andreiturcila@MacBookPro.lan>
2025-04-29 14:39:48 +0100

    adding CLI command to run query

--- athena.tf
+++ athena.tf
@@ -73,13 +73,13 @@ EOF
   workgroup = aws_athena_workgroup.access_logs.name
 }
 
-# resource "null_resource" "execute_query" {
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       aws athena start-query-execution \
-#         --query-string "${aws_athena_named_query.create_athena_s3_table.query}" \
-#         --query-execution-context Database=${aws_athena_named_query.create_athena_s3_query.database} \
-#         --result-configuration OutputLocation="s3://${aws_s3_bucket.cloudfront_logging_bucket.bucket}/query_results/create_athena_s3_logs_table/" \
-#     EOT
-#   }
-# }
+resource "null_resource" "execute_query" {
+  provisioner "local-exec" {
+    command = <<EOT
+      aws athena start-query-execution \
+        --query-string '${aws_athena_named_query.create_athena_s3_table.query}' \
+        --query-execution-context Database=${aws_athena_named_query.create_athena_s3_query.database} \
+        --result-configuration OutputLocation='s3://${aws_s3_bucket.cloudfront_logging_bucket.bucket}/query_results/create_athena_s3_logs_table/' \
+    EOT
+  }
+}
