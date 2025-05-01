510116bac4069749103207f6a7d2de1b4576d20d
Andrei Turcila <andreiturcila@MacBookPro.lan>
2025-05-01 09:52:19 +0100

    adding athena permission fix

--- athena.tf
+++ athena.tf
@@ -76,7 +76,7 @@ EOF
 resource "null_resource" "execute_query" {
   provisioner "local-exec" {
     command = <<EOT
-    echo 'ARN: ${aws_athena_named_query.create_athena_s3_table.id}'
+    $(aws sts assume-role --role-arn arn:aws:iam::${var.target_account_id}:role/aws_shared_concourse --role-session-name aws-athena-run-query --query 'Credentials.[`export#AWS_ACCESS_KEY_ID=`,AccessKeyId,`#AWS_SECRET_ACCESS_KEY=`,SecretAccessKey,`#AWS_SESSION_TOKEN=`,SessionToken]' --output text | sed $'s/\t//g' | sed 's/#/ /g')
       aws athena start-query-execution \
         --query-string '${aws_athena_named_query.create_athena_s3_table.query}' \
         --query-execution-context Database=${aws_athena_named_query.create_athena_s3_table.database} \
@@ -85,45 +85,45 @@ resource "null_resource" "execute_query" {
   }
 }
 
-resource "aws_iam_policy" "athena_execute_query_policy" {
-#checkov:skip=CKV_AWS_355:testing execute_query, fix to be implemented
-#checkov:skip=CKV_AWS_290:testing excute_query, fix to be implemented
+# resource "aws_iam_policy" "athena_execute_query_policy" {
+# #checkov:skip=CKV_AWS_355:testing execute_query, fix to be implemented
+# #checkov:skip=CKV_AWS_290:testing excute_query, fix to be implemented
 
-  name        = "AthenaExecuteQueryPolicy"
-  description = "Policy to allow Athena query execution and S3 access for query results"
-  policy      = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "athena:StartQueryExecution",
-          "athena:GetQueryExecution",
-          "athena:GetQueryResults"
-        ],
-        Effect   = "Allow",
-        Resource = "*"
-        # Resource = [
-        #   "arn:aws:athena:::${aws_athena_workgroup.access_logs.name}",
-        #   "arn:aws:athena:::${aws_athena_database.access_logs.name}",
-        #   "arn:aws:athena:::${aws_athena_named_query.create_athena_s3_table.id}"
-        # ]
-      },
-      {
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Effect   = "Allow",
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.cloudfront_logging_bucket.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.cloudfront_logging_bucket.bucket}/*"
-        ]
-      }
-    ]
-  })
-}
-resource "aws_iam_role_policy_attachment" "attach_athena_policy" {
-  role       = aws_iam_role.cloudwatch_global.name
-  policy_arn = aws_iam_policy.athena_execute_query_policy.arn
-}
+#   name        = "AthenaExecuteQueryPolicy"
+#   description = "Policy to allow Athena query execution and S3 access for query results"
+#   policy      = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = [
+#           "athena:StartQueryExecution",
+#           "athena:GetQueryExecution",
+#           "athena:GetQueryResults"
+#         ],
+#         Effect   = "Allow",
+#         Resource = "*"
+#         # Resource = [
+#         #   "arn:aws:athena:::${aws_athena_workgroup.access_logs.name}",
+#         #   "arn:aws:athena:::${aws_athena_database.access_logs.name}",
+#         #   "arn:aws:athena:::${aws_athena_named_query.create_athena_s3_table.id}"
+#         # ]
+#       },
+#       {
+#         Action = [
+#           "s3:PutObject",
+#           "s3:GetObject",
+#           "s3:ListBucket"
+#         ],
+#         Effect   = "Allow",
+#         Resource = [
+#           "arn:aws:s3:::${aws_s3_bucket.cloudfront_logging_bucket.bucket}",
+#           "arn:aws:s3:::${aws_s3_bucket.cloudfront_logging_bucket.bucket}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
+# resource "aws_iam_role_policy_attachment" "attach_athena_policy" {
+#   role       = aws_iam_role.cloudwatch_global.name
+#   policy_arn = aws_iam_policy.athena_execute_query_policy.arn
+# }
