cca912e7dfad6beef3ccbf7310bce855ff6f4879
Andrei Turcila <andreiturcila@MacBookPro.lan>
2025-05-01 11:03:38 +0100

    adding athena permission fix

--- athena.tf
+++ athena.tf
@@ -72,13 +72,13 @@ TBLPROPERTIES ( 'skip.header.line.count'='2' )
 EOF
   workgroup = aws_athena_workgroup.access_logs.name
 }
-
+#  --query-string "${aws_athena_named_query.create_athena_s3_table.query}" \
 resource "null_resource" "execute_query" {
   provisioner "local-exec" {
     command = <<EOT
     $(aws sts assume-role --role-arn arn:aws:iam::${var.target_account_id}:role/aws_shared_concourse --role-session-name aws-athena-run-query --query 'Credentials.[`export#AWS_ACCESS_KEY_ID=`,AccessKeyId,`#AWS_SECRET_ACCESS_KEY=`,SecretAccessKey,`#AWS_SESSION_TOKEN=`,SessionToken]' --output text | sed $'s/\t//g' | sed 's/#/ /g')
       aws athena start-query-execution \
-        --query-string "${aws_athena_named_query.create_athena_s3_table.query}" \
+        --query-string CREATE EXTERNAL TABLE IF NOT EXISTS ${each.key} AS ${each.value} \
         --query-execution-context Database=${aws_athena_named_query.create_athena_s3_table.database} \
         --result-configuration OutputLocation='s3://${aws_s3_bucket.cloudfront_logging_bucket.bucket}/query_results/create_athena_s3_logs_table/' \
     EOT
