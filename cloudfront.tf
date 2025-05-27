ec68d969b3f8f54aafd24b8c42336b040fb3dd01
Andrei Turcila <andreiturcila@MacBookPro.lan>
2025-05-27 15:50:58 +0100

    adding cf field level encryption

--- cloudfront.tf
+++ cloudfront.tf
@@ -203,7 +203,7 @@ data "aws_secretsmanager_secret" "public_key_secret" {
 #}
 data "aws_secretsmanager_secret_version" "cloudfront_secret" {
   secret_id     = data.aws_secretsmanager_secret.public_key_secret[0].id
-  count         = "uploader-cloudfront-encryption-key" ? 1 : 0
+  count         = "uploader-cloudfront-encryption-key" == "uploader-cloudfront-encryption-key" ? 1 : 0
 }
 
 resource "aws_key_pair" "cloudfront_key_pair" {
