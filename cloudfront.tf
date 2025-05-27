bed66d6685b22cb7cf9ad15fe3b18425bf372d92
Andrei Turcila <andreiturcila@MacBookPro.lan>
2025-05-27 15:39:38 +0100

    adding cf field level encryption

--- cloudfront.tf
+++ cloudfront.tf
@@ -186,19 +186,34 @@ data "aws_secretsmanager_secret" "public_key_secret" {
   name = "uploader-cloudfront-encryption-key"
 }
 
+#data "aws_secretsmanager_secret_version" "cloudfront_secret" {
+  #secret_id     = data.aws_secretsmanager_secret.public_key_secret[0].id
+  #count         = "uploader-cloudfront-encryption-key" ? 1 : 0
+#}
+
+#resource "aws_key_pair" "cloudfront_key_pair" {
+ # public_key = "${data.aws_secretsmanager_secret_version.public_key_secret.secret_string}"
+  #key_name   = "uploader-cloudfront-encryption-key"
+#}
+
+#resource "aws_cloudfront_public_key" "cloudfront_encryption_key" {
+ # comment     = "Cloudfront encryption key"
+  #encoded_key = aws_secretsmanager_secret_version.cloudfront_secret.secret_string
+  #name        = "cloudfront-encryption-key"
+#}
 data "aws_secretsmanager_secret_version" "cloudfront_secret" {
   secret_id     = data.aws_secretsmanager_secret.public_key_secret[0].id
   count         = "uploader-cloudfront-encryption-key" ? 1 : 0
 }
 
 resource "aws_key_pair" "cloudfront_key_pair" {
-  public_key = "${data.aws_secretsmanager_secret_version.public_key_secret.secret_string}"
+  public_key = data.aws_secretsmanager_secret_version.cloudfront_secret[0].secret_string
   key_name   = "uploader-cloudfront-encryption-key"
 }
 
 resource "aws_cloudfront_public_key" "cloudfront_encryption_key" {
   comment     = "Cloudfront encryption key"
-  encoded_key = aws_secretsmanager_secret_version.cloudfront_secret.secret_string
+  encoded_key = data.aws_secretsmanager_secret_version.cloudfront_secret[0].secret_string
   name        = "cloudfront-encryption-key"
 }
 
