6197c5f97cffa3e0848f224ba78138bbc9105c22
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2025-06-03 11:47:54 +0100

    add initial behaviour test

---
+++ features/step_definitions/step_defs.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('I have navigated to the uploader page', function () {
+webURL = "https://uploader.ingest-dev.aws.onsdigital.uk/council-tax/E00000000-Test&Test.html"
+
+});
+
+// When('', function () {
+//   // code here;
+// });
+
+// Then('', function (expectedAnswer) {
+//   // code here;
+// });
---
+++ features/uploader.feature
@@ -0,0 +1,6 @@
+Feature: Error handling in the uploader
+
+  Scenario: Error when uploading a file
+    Given I have navigated to the uploader page
+# When I attempt to upload a file that is too large
+# Then I should see an error message indicating the file is too large
