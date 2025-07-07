efb86bca1bc42d0c54faeb2d259e3ab99a440a7b
Marc Cattini <marc.cattini@ons.gov.uk>
2025-07-07 18:52:16 +0100

    first 11 UI tests

--- features/step_definitions/step_defs.js
+++ features/step_definitions/step_defs.js
@@ -13,7 +13,7 @@ Given('I have navigated to the uploader page', async function () {
 });
 
 //WHEN //
-When('I upload an extract file', async function () {
+When('I upload a correctly named non-zero byte extract file that matches the URL LAD code', async function () {
     let fileInput = await this.driver.findElement(By.id('fileOne'));
     const path = require('path');
     let filePath = path.resolve(process.cwd(), 'features/test_files/CTAX_EXTRACT_E00000000_20250131.csv');
@@ -21,10 +21,58 @@ When('I upload an extract file', async function () {
     await fileInput.sendKeys(filePath);
 });
 
-When('I upload a manifest file', async function () {
+When('I upload a correctly named non-zero byte manifest file that matches the URL LAD code', async function () {
     let fileInput = await this.driver.findElement(By.id('fileTwo'));
     const path = require('path');
     let filePath = path.resolve(process.cwd(), 'features/test_files/CTAX_MANI_E00000000_20250131.csv');
+
+    await fileInput.sendKeys(filePath);
+});
+
+When('I upload an incorrectly named non-zero byte manifest file', async function () {
+    let fileInput = await this.driver.findElement(By.id('fileTwo'));
+    const path = require('path');
+    let filePath = path.resolve(process.cwd(), 'features/test_files/CTAX_MANI_E00000000_202501.csv');
+
+    await fileInput.sendKeys(filePath);
+});
+
+When('I upload an incorrectly named non-zero byte extract file', async function () {
+    let fileInput = await this.driver.findElement(By.id('fileOne'));
+    const path = require('path');
+    let filePath = path.resolve(process.cwd(), 'features/test_files/CTAX_EXTRACT_E00000000_202501.csv');
+
+    await fileInput.sendKeys(filePath);
+});
+
+When('I upload a non-zero byte manifest file that does not have a .csv extension', async function () {
+    let fileInput = await this.driver.findElement(By.id('fileTwo'));
+    const path = require('path');
+    let filePath = path.resolve(process.cwd(), 'features/test_files/CTAX_MANI_E00000000_20250131');
+
+    await fileInput.sendKeys(filePath);
+});
+
+When('I upload a non-zero byte extract file that does not have a .csv extension', async function () {
+    let fileInput = await this.driver.findElement(By.id('fileOne'));
+    const path = require('path');
+    let filePath = path.resolve(process.cwd(), 'features/test_files/CTAX_EXTRACT_E00000000_20250131');
+
+    await fileInput.sendKeys(filePath);
+});
+
+When('I upload a correctly named non-zero byte manifest file that does not match the URL LAD Code', async function () {
+    let fileInput = await this.driver.findElement(By.id('fileTwo'));
+    const path = require('path');
+    let filePath = path.resolve(process.cwd(), 'features/test_files/CTAX_MANI_E00000001_20250131.csv');
+
+    await fileInput.sendKeys(filePath);
+});
+
+When('I upload a correctly named non-zero byte extract file that does not match the URL LAD Code', async function () {
+    let fileInput = await this.driver.findElement(By.id('fileOne'));
+    const path = require('path');
+    let filePath = path.resolve(process.cwd(), 'features/test_files/CTAX_EXTRACT_E00000001_20250131.csv');
 
     await fileInput.sendKeys(filePath);
 });
@@ -36,6 +84,21 @@ When('I click "Submit"', async function () {
 });
 
 // THEN //
+Then('I should see the extract file input field', async function () {
+    let extractFileInput = await this.driver.findElement(By.id('fileOne'));
+    assert.ok(extractFileInput.isDisplayed());
+});
+
+Then('I should see the mani file input field', async function () {
+    let extractFileInput = await this.driver.findElement(By.id('fileOne'));
+    assert.ok(extractFileInput.isDisplayed());
+});
+
+Then('I should see the submit button', async function () {
+    let submitButton = await this.driver.findElement(By.className('ons-btn'));
+    assert.ok(submitButton.isDisplayed());
+});
+
 Then('I should see a "Success" message', async function () {
     await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Success")]')), 5000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
@@ -48,7 +111,28 @@ Then('I should see a "You need to fill in both fields" message', async function 
     await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "You need to fill in both fields")]')), 5000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
 
-    assert.ok(pageText.includes("You need to fill in both fields"), 'The text "YI should see a "You need to fill in both fields" message" was not found on the page');
+    assert.ok(pageText.includes("You need to fill in both fields"), 'The text "You need to fill in both fields" message" was not found on the page');
+});
+
+Then('I should see a “Mani File name does not follow the right pattern” message', async function () {
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Mani File name does not follow the right pattern")]')), 5000);
+    let pageText = await this.driver.findElement(By.tagName('body')).getText();
+
+    assert.ok(pageText.includes("Mani File name does not follow the right pattern"), 'The text "Mani File name does not follow the right pattern" message was not found on the page');
+});
+
+Then('I should see a “Extract File name does not follow the right pattern” message', async function () {
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Extract File name does not follow the right pattern")]')), 5000);
+    let pageText = await this.driver.findElement(By.tagName('body')).getText();
+
+    assert.ok(pageText.includes("Extract File name does not follow the right pattern"), 'The text "Extract File name does not follow the right pattern" message was not found on the page');
+});
+
+Then('I should see a “File name does not contain matching LAD code” message', async function () {
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "File name does not contain matching LAD code")]')), 5000);
+    let pageText = await this.driver.findElement(By.tagName('body')).getText();
+
+    assert.ok(pageText.includes("File name does not contain matching LAD code"), 'The text "File name does not contain matching LAD code" message was not found on the page');
 });
 
 Then('I should see a "You need to add a Mani file" message', async function () {
--- features/uploader.feature
+++ features/uploader.feature
@@ -1,22 +1,76 @@
 Feature: Upload files via the uploader page
+  Scenario: Page loads correctly
+    Given I have navigated to the uploader page
+    Then I should see the extract file input field
+    And I should see the mani file input field
+    And I should see the submit button
 
   Scenario: Successfully upload files (happy path)
     Given I have navigated to the uploader page
-    When I upload an extract file
-    And I upload a manifest file
+    When I upload a correctly named non-zero byte extract file that matches the URL LAD code
+    And I upload a correctly named non-zero byte manifest file that matches the URL LAD code
     And I click "Submit"
     Then I should see a "Success" message
 
+  Scenario: Fail to upload files due to both files missing
+    Given I have navigated to the uploader page
+    And I click "Submit"
+    Then I should see a "You need to fill in both fields" message
+    And I should see a "You need to add a Extract file" message
+    And I should see a "You need to add a Mani file" message
+
   Scenario: Fail to upload files due to missing manifest file
     Given I have navigated to the uploader page
-    When I upload an extract file
+    When I upload a correctly named non-zero byte extract file that matches the URL LAD code
     And I click "Submit"
     Then I should see a "You need to fill in both fields" message
     And I should see a "You need to add a Mani file" message
 
   Scenario: Fail to upload files due to missing extract file
     Given I have navigated to the uploader page
-    When I upload a manifest file
+    When I upload a correctly named non-zero byte manifest file that matches the URL LAD code
     And I click "Submit"
     Then I should see a "You need to fill in both fields" message
     And I should see a "You need to add a Extract file" message
+
+  Scenario: Upload fails due to manifest file not following naming convention
+ 	  Given I have navigated to the uploader page
+    When I upload a correctly named non-zero byte extract file that matches the URL LAD code
+    And I upload an incorrectly named non-zero byte manifest file
+    And I click "Submit"
+    Then I should see a “Mani File name does not follow the right pattern” message
+
+  Scenario: Upload fails due to extract file not following naming convention
+ 	  Given I have navigated to the uploader page
+    When I upload a correctly named non-zero byte manifest file that matches the URL LAD code
+    And I upload an incorrectly named non-zero byte extract file
+    And I click "Submit"
+    Then I should see a “Extract File name does not follow the right pattern” message
+
+  Scenario: Upload fails due to manifest file not having .csv file extension
+ 	  Given I have navigated to the uploader page
+    When I upload a correctly named non-zero byte extract file that matches the URL LAD code
+    And I upload a non-zero byte manifest file that does not have a .csv extension
+    And I click "Submit"
+    Then I should see a “Mani File name does not follow the right pattern” message
+
+  Scenario: Upload fails due to extract file not having .csv file extension
+ 	  Given I have navigated to the uploader page
+    When I upload a correctly named non-zero byte manifest file that matches the URL LAD code
+    And I upload a non-zero byte extract file that does not have a .csv extension
+    And I click "Submit"
+    Then I should see a “Extract File name does not follow the right pattern” message
+
+  Scenario: Upload fails due to manifest file not matching URL LAD Code
+ 	  Given I have navigated to the uploader page
+    When I upload a correctly named non-zero byte extract file that matches the URL LAD code
+    And I upload a correctly named non-zero byte manifest file that does not match the URL LAD Code
+    And I click "Submit"
+    Then I should see a “File name does not contain matching LAD code” message
+
+  Scenario: Upload fails due to extract file not matching URL LAD Code
+ 	  Given I have navigated to the uploader page
+    When I upload a correctly named non-zero byte manifest file that matches the URL LAD code
+    And I upload a correctly named non-zero byte extract file that does not match the URL LAD Code
+    And I click "Submit"
+    Then I should see a “File name does not contain matching LAD code” message
