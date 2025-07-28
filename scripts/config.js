a86de09db650fe6444851f85e040a87c6a4a5739
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2025-07-28 11:59:37 +0100

    feat: add flag for maintenance mode

---
+++ scripts/config.js
@@ -0,0 +1,8 @@
+// Toggle this to true to enable maintenance mode
+// const maintenanceMode = false;
+const maintenanceMode = true;
+
+// Export for use in other scripts if needed
+if (typeof window !== 'undefined') {
+  window.maintenanceMode = maintenanceMode;
+}
