da5ae062ba36dffdf04cbf3e09da1872b8978ce5
Siva Mandalapu <sivamandalapu@MacBookPro-2.powerhub>
2025-09-17 16:29:33 +0100

    reverting  index.js

--- scripts/second_index.js
+++ scripts/second_index.js
@@ -2,12 +2,8 @@ async function handler(event) {
     var request = event.request;
     var uri = request.uri;
     
-    // For council-tax pages, decode URL-encoded characters to match S3 object keys
-    if (uri.startsWith('/council-tax/')) {
-        request.uri = decodeURIComponent(uri);
-    }
     // Check whether the URI is missing a file name.
-    else if (uri.endsWith('/')) {
+    if (uri.endsWith('/')) {
         request.uri += 'index.html';
     } 
     // Check whether the URI is missing a file extension.
