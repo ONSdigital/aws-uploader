ae2318c2e6a321d9388584f356a0823e6fe17e8a
Siva Mandalapu <sivamandalapu@MacBookPro-2.powerhub>
2025-09-17 16:58:35 +0100

    adding  to  index.js

--- scripts/second_index.js
+++ scripts/second_index.js
@@ -1,17 +1,14 @@
 async function handler(event) {
     var request = event.request;
-    var uri = decodeURIComponent(request.uri);
+    var uri = request.uri;
     
     // Check whether the URI is missing a file name.
     if (uri.endsWith('/')) {
-        request.uri = uri + 'index.html';
+        request.uri += 'index.html';
     } 
     // Check whether the URI is missing a file extension.
     else if (!uri.includes('.')) {
-        request.uri = uri + '/index.html';
-    }
-    else {
-        request.uri = uri;
+        request.uri += '/index.html';
     }
 
     return request;
