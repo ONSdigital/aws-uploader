1217c4f3a079921fa39e8cd984cedd6b18f7d1cc
Siva Mandalapu <sivamandalapu@MacBookPro-2.powerhub>
2025-09-17 16:14:06 +0100

    modifying index.js

--- scripts/second_index.js
+++ scripts/second_index.js
@@ -2,8 +2,12 @@ async function handler(event) {
     var request = event.request;
     var uri = request.uri;
     
+    // For council-tax pages, decode URL-encoded characters to match S3 object keys
+    if (uri.startsWith('/council-tax/')) {
+        request.uri = decodeURIComponent(uri);
+    }
     // Check whether the URI is missing a file name.
-    if (uri.endsWith('/')) {
+    else if (uri.endsWith('/')) {
         request.uri += 'index.html';
     } 
     // Check whether the URI is missing a file extension.
