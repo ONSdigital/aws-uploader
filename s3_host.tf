0b97b31c6744ccf20dca40ce8dd5467d75334718
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2025-11-11 07:44:14 +0000

    remove unused locals

--- s3_host.tf
+++ s3_host.tf
@@ -56,26 +56,6 @@ data "aws_iam_policy_document" "uploader_bucket" {
   }
 }
 
-
-locals {
-  file_submission_js = templatefile("${path.module}/scripts/file_submission.js", {
-    api_url = aws_apigatewayv2_stage.api.invoke_url
-  })
-
-  multi_file_submission_js = templatefile("${path.module}/scripts/multi_file_submission.js", {
-    api_url = aws_apigatewayv2_stage.api.invoke_url
-  })
-
-  E12345678-council-rendered-html = templatefile("${path.module}/scripts/template/council-tax-template.html", {
-    council_name = "Council Tax"
-    lad_code     = "E12345678"
-  })
-
-  E07000175-council-rendered-html = templatefile("${path.module}/scripts/template/council-tax-template.html", {
-    council_name = "Council Newark & Sherwood"
-    lad_code     = "E07000175"
-  })
-}
 resource "aws_s3_bucket_policy" "uploader_bucket" {
   bucket = module.ons_upload_bucket.bucket_id
   policy = data.aws_iam_policy_document.uploader_bucket.json
