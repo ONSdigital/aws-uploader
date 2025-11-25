5bb357188ebd8ae53c1d0f6114d6152666495fe1
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2025-11-25 13:36:15 +0000

    bump wait

--- features/step_definitions/step_defs.js
+++ features/step_definitions/step_defs.js
@@ -122,49 +122,49 @@ Then('I should see a "Success" message', async function () {
 });
 
 Then('I should see a "You need to fill in both fields" message', async function () {
-    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "You need to fill in both fields")]')), 5000);
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "You need to fill in both fields")]')), 10000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
 
     assert.ok(pageText.includes("You need to fill in both fields"), 'The text "You need to fill in both fields" message" was not found on the page');
 });
 
-Then('I should see a “Mani File name does not follow the right pattern” message', async function () {
-    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Mani File name does not follow the right pattern")]')), 5000);
+Then('I should see a "Mani File name does not follow the right pattern" message', async function () {
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Mani File name does not follow the right pattern")]')), 10000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
 
     assert.ok(pageText.includes("Mani File name does not follow the right pattern"), 'The text "Mani File name does not follow the right pattern" message was not found on the page');
 });
 
-Then('I should see a “Extract File name does not follow the right pattern” message', async function () {
-    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Extract File name does not follow the right pattern")]')), 5000);
+Then('I should see a "Extract File name does not follow the right pattern" message', async function () {
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Extract File name does not follow the right pattern")]')), 10000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
 
     assert.ok(pageText.includes("Extract File name does not follow the right pattern"), 'The text "Extract File name does not follow the right pattern" message was not found on the page');
 });
 
-Then('I should see a “File name does not contain matching LAD code” message', async function () {
-    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "File name does not contain matching LAD code")]')), 5000);
+Then('I should see a "File name does not contain matching LAD code" message', async function () {
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "File name does not contain matching LAD code")]')), 10000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
 
     assert.ok(pageText.includes("File name does not contain matching LAD code"), 'The text "File name does not contain matching LAD code" message was not found on the page');
 });
 
 Then('I should see a "You need to add a Mani file" message', async function () {
-    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "You need to add a Mani file")]')), 5000);
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "You need to add a Mani file")]')), 10000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
 
     assert.ok(pageText.includes("You need to add a Mani file"), 'The text "You need to add a Mani file" was not found on the page');
 });
 
 Then('I should see a "You need to add a Extract file" message', async function () {
-    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "You need to add a Extract file")]')), 5000);
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "You need to add a Extract file")]')), 10000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
 
     assert.ok(pageText.includes("You need to add a Extract file"), 'The text "You need to add a Extract file" was not found on the page');
 });
 
-Then('I should see a “File names do not match” message', async function () {
-    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "File names do not match")]')), 5000);
+Then('I should see a "File names do not match" message', async function () {
+    await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "File names do not match")]')), 10000);
     let pageText = await this.driver.findElement(By.tagName('body')).getText();
 
     assert.ok(pageText.includes("File names do not match"), 'The text "File names do not match" was not found on the page');
