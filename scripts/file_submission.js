f619cc9b4fd0f69f4816fc6ef8ac9c956f3c99be
Elinor <46898565+elthorne@users.noreply.github.com>
2026-02-11 14:43:26 +0000

    feat: Remove duplicated error messages
    refs: ISC-666

--- scripts/file_submission.js
+++ scripts/file_submission.js
@@ -27,10 +27,8 @@ function commonErrorStyle(errorCount) {
   if (errorCount === 1) {
     errorsTitle.innerHTML =
       '<h2 class="ons-panel__title ons-u-fs-r--b">There is 1 problem with your answer</h2>';
-    errorsText.textContent = "There is 1 problem with your answer";
   } else {
     errorsTitle.innerHTML = `<h2 class="ons-panel__title ons-u-fs-r--b">There are $${errorCount} problems with your answer</h2>`;
-    errorsText.textContent = `There are $${errorCount} problems with your answer`;
   }
 }
 
