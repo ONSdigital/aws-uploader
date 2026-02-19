0e798ba2fda8fcd8f85add2359bf83a2b92d7e92
Elinor <46898565+elthorne@users.noreply.github.com>
2026-02-19 12:16:42 +0000

    fix: The project name must be on solid word without symbols and sapces or numbers
    refs: ISC-651

--- alerts.tf
+++ alerts.tf
@@ -1,7 +1,7 @@
 module "ct-uploader-alerts" {
   source = "git::https://github.com/ONSdigital/aws-alerts?ref=1.3.10"
 
-  project_name              = "ct_uploader"
+  project_name              = "ct-uploader"
   environment               = var.environment
   slack_webhook_security    = local.ct_uploader_slack.security_webhook
   slack_webhook_operational = local.ct_uploader_slack.operational_webhook
