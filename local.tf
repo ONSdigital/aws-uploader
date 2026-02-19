cfbe025cf6526b7c7f6255b85f1057edb38a55d6
Elinor <46898565+elthorne@users.noreply.github.com>
2026-02-19 11:59:24 +0000

    feat: integrate alerts
    refs: ISC-651

--- local.tf
+++ local.tf
@@ -2,3 +2,18 @@ locals {
   website_address = "uploader.${var.domain_name}"
   s3_domain       = "https://${module.ons_upload_ingest_bucket.bucket_id}.s3.${data.aws_region.current.name}.amazonaws.com"
 }
+
+# alerting webhook
+data "aws_secretsmanager_secret" "secretsmanager_secret_cia_ingest_slack" {
+  name = "ct_uploader_slack"
+}
+
+data "aws_secretsmanager_secret_version" "secretsmanager_secret_version_ct_uploader_slack" {
+  secret_id = data.aws_secretsmanager_secret.secretsmanager_secret_ct_uploader_slack.id
+}
+
+locals {
+  ct_uploader_slack = jsondecode(
+    data.aws_secretsmanager_secret_version.secretsmanager_secret_version_ct_uploader_slack.secret_string
+  )
+}
