f552de9f68c6105fb231d51ae214f74245e69a0a
Elinor <46898565+elthorne@users.noreply.github.com>
2026-02-23 12:23:46 +0000

    fix: set deploy_cost_anomaly_detector to false
    refs: ISC-651

--- alerts.tf
+++ alerts.tf
@@ -1,8 +1,10 @@
 module "ct-uploader-alerts" {
-  source = "git::https://github.com/ONSdigital/aws-alerts?ref=1.3.10"
+  source = "git::https://github.com/ONSdigital/aws-alerts?ref=1.3.11"
 
   project_name              = "ctuploader"
   environment               = var.environment
   slack_webhook_security    = local.ct_uploader_slack.security_webhook
   slack_webhook_operational = local.ct_uploader_slack.operational_webhook
+
+  deploy_cost_anomaly_detector = false
 }
