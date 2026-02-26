816e188e3cf803cd039950271f06fa5e7beb8fbf
Elinor <46898565+elthorne@users.noreply.github.com>
2026-02-26 14:18:37 +0000

    fix: shorten project prefix from "ctuploader" to "uploader" to meet IAM role name limits
    Terraform failed to create the IAM role because
    the generated name (ctuploaderpreprod_alerts_lambda_sns_to_slack_operational_iam_role)
    exceeded AWS's 64-character limit for IAM role
    names.
    
    refs: ISC-651

--- alerts.tf
+++ alerts.tf
@@ -1,7 +1,7 @@
 module "ct-uploader-alerts" {
   source = "git::https://github.com/ONSdigital/aws-alerts?ref=1.3.13"
 
-  project_name              = "ctuploader"
+  project_name              = "uploader"
   environment               = var.environment
   slack_webhook_security    = local.ct_uploader_slack.security_webhook
   slack_webhook_operational = local.ct_uploader_slack.operational_webhook
