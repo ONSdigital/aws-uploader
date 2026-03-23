ea57967dc052da556e0b78f02eb9ac9050aefda3
Elinor <46898565+elthorne@users.noreply.github.com>
2026-03-23 11:18:10 +0000

    fix: announce error summary to screen readers
    refs: ISC-797

--- scripts/file_submission.js
+++ scripts/file_submission.js
@@ -26,16 +26,17 @@ function commonErrorStyle(errorCount) {
   const errorsTitle = document.getElementById("errors-list-title");
   const errorsText = document.getElementById("errors-list-text");
 
-  errorsList.style.display = "block";
-
   if (errorCount === 1) {
-    errorsTitle.innerHTML =
-      '<h2 class="ons-panel__title ons-u-fs-r--b">There is 1 problem with your answer</h2>';
+    errorsTitle.innerHTML = '<h2 class="ons-panel__title ons-u-fs-r--b">There is 1 problem with your answer</h2>';
     errorsText.textContent = "There is 1 problem with your answer";
   } else {
-    errorsTitle.innerHTML = `<h2 class="ons-panel__title ons-u-fs-r--b">There are $${errorCount} problems with your answer</h2>`;
-    errorsText.textContent = `There are $${errorCount} problems with your answer`;
+    errorsTitle.innerHTML = `<h2 class="ons-panel__title ons-u-fs-r--b">There are ${errorCount} problems with your answer</h2>`;
+    errorsText.textContent = `There are ${errorCount} problems with your answer`;
   }
+
+  errorsList.style.display = "block";
+
+  setTimeout(() => errorsList.focus(), 100);
 }
 
 function bothFilesErrorStyle() {
